b6b6081a314ed5847230e4d7aaf6b422b01a23d3
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2024-11-28 13:20:01 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Artur Honcharuk on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
