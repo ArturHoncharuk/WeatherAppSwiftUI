c524722e6637d5513d667e3ead88e8b16cd9ba24
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2024-11-28 13:25:59 +0200

    feat: add location manager

---
+++ WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,36 @@
+//
+//  Location.swift
+//  WeatherApp
+//
+//  Created by Artur Honcharuk on 28.11.2024.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while getting location", error)
+        isLoading = false
+    }
+}
